b5b4fa6c13f60f476b82394ff611a5622ad4fb5c
Micah Resnick <resnickmicah@gmail.com>
2025-08-24 22:18:39 -0400

    Move around a bunch of client/server stuff
    All this MSFT boilerplate looks the same lol

--- BlazorSignalRApp/Program.cs
+++ BlazorSignalRApp/Program.cs
@@ -1,5 +1,6 @@
-using BlazorSignalRApp.Client.Pages;
 using BlazorSignalRApp.Components;
+using Microsoft.AspNetCore.ResponseCompression;
+using BlazorSignalRApp.Hubs;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,7 +8,16 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveWebAssemblyComponents();
 
+builder.Services.AddSignalR();
+
+builder.Services.AddResponseCompression(opts =>
+{
+   opts.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(
+       [ "application/octet-stream" ]);
+});
+
 var app = builder.Build();
+app.UseResponseCompression();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -30,5 +40,7 @@ app.MapStaticAssets();
 app.MapRazorComponents<App>()
     .AddInteractiveWebAssemblyRenderMode()
     .AddAdditionalAssemblies(typeof(BlazorSignalRApp.Client._Imports).Assembly);
+
+app.MapHub<ChatHub>("/chathub");
 
 app.Run();
